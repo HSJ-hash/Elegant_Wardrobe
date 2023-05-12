6629f76dcdb5c214c5529c69ac0b4d7ac4d85123
Heshani <heshanisamadhi25@gmail.com>
2023-05-12 17:34:47 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="foot.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Crete+Round&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+
+</head>
+<body>
+<footer>
+      <div class="container">
+        <div class="sec aboutus">
+          <h2>ELEGANT WARDROBE</h2>
+          <p>Our passion for travel drives our ability to provide world-class, personalized service and quality products. To provide world class Clothing with outstanding service to our Customers around the country. To attain highest level of efficiency, integrity and honesty.</p>
+          <ul class="sci">
+            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+        
+        <div class="quicklink">
+          <h2>Quick Links</h2>
+          <ul>
+              <li><a href="Male_cloths.php">Male</a></li>
+              <li><a href="Female_cloths.php">Women</a></li>
+              <li><a href="Kids_cloths.php">Kid</a></li>
+          </ul>
+        </div>
+
+        <div class="contact">
+          <h2>Contact Info</h2>
+          <ul class="info">
+            <li>
+
+            <span><i class="fa fa-map" aria-hidden="true"></i></span>
+            <span>647 Linda Street <br>
+            Colombo, <br> Sri Lanka</span>
+
+            </li>
+
+            <li>
+
+              <span><i class="fa fa-phone" aria-hidden="true"></i></span>
+              <p><a href="tel:0114527896">011 452 7896</a></p>
+  
+              </li>
+
+              <li>
+
+                <span><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                <p><a href="mailto:info@elegant.com">info@elegant.com</a></p>
+    
+              </li>
+
+          </ul>
+
+          </div>
+
+      </div>
+    </footer>
+
+    <div class="copyright">
+
+      <p>Copyright © 2022 Elegant Wardrobe. Developed and Designed by Wanda Technologies.</p>
+
+    </div>
+</body>
